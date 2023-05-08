4f1b467a79638e214fc3644ed22acbcc80220ec8
Kiril Bangachev <51961981+KirilBangachev@users.noreply.github.com>
2023-05-08 14:50:26 -0400

    Add optimal rotation file

---
+++ SphericalWorld/RotateYtoX.m
@@ -0,0 +1,7 @@
+function [Image,TotalL2Distance,ElementWiseL2Distance] = RotateYtoX(X,Y)
+    prod = X*Y';
+    [U,S,V] = svd(prod);
+    Image = U*V'*Y;
+    TotalL2Distance = norm(X - Image, "fro");
+    ElementWiseL2Distance = vecnorm(X - Image,2);
+end
