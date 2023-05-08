6e146294590d924b79b382fc23637412ff7be3bc
Kiril Bangachev <51961981+KirilBangachev@users.noreply.github.com>
2023-05-08 14:44:10 -0400

    Delete RotateYtoX.m

--- SphericalWorld/RotateYtoX.m
+++
@@ -1,8 +0,0 @@
-function [Image,TotalL2Distance,ElementWiseL2Distance] = RotateYtoX(X,Y)
-    prod = X*Y';
-    [U,S,V] = svd(prod);
-    Image = U*V'*Y;
-    TotalL2Distance = norm(X - Image, "fro");
-    ElementWiseL2Distance = vecnorm(X - Image,2);
-    size(ElementWiseL2Distance)
-end
