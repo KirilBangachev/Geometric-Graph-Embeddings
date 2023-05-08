2de381ed5ba203baac983683b049d51e721c2dea
Kiril Bangachev <51961981+KirilBangachev@users.noreply.github.com>
2023-05-08 14:43:49 -0400

    Delete rotate320locatiosn.m

--- SphericalWorld/rotate320locatiosn.m
+++
@@ -1,6 +0,0 @@
-locations_320 = importdata("City Data/inferred_city_locations_320.txt");
-locations_true = importdata("city_loactions");
-
-locations_320_rotated = RotateYtoX(locations_true(1:320,:)', locations_320)
-save("City Data/rotated_city_locations_320.txt", "locations_320_rotated","-ascii")
-
